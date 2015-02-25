fc7a5b8be55ada4bb514be2b5fe22d33d9b65c31
Arun V Jose <arunvalluthadam@gmail.com>
2015-02-25 09:24:21 +0530

    configuring urls

--- twitter_api/urls.py
+++ twitter_api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from twitter_api import settings
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,7 +11,10 @@ urlpatterns = patterns('',
     url(r'^twitter-ajax/$', 'twitter_feed.views.twitter_ajax', name='twitter_ajax'),
     url(r'^github-ajax/$', 'twitter_feed.views.github_ajax', name='github_ajax'),
 
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
